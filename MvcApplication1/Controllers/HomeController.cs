f68ffc68f3f5d95f79ed5cb348d6cfcc9cc3038b
Fabian Nicollier <fabian.nicollier@ed47.com>
2013-09-19 08:42:02 +0200

    Initial commit

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/        
+        public ActionResult Index()
+        {
+            Membership.CreateUser("tets", "testtesttesttest123");
+            return View();
+        }
+
+    }
+}
